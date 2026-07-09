0dcfad917d206d6a321a13dcc3fb69cc393314af
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-09 07:44:21 +0330

    Completing the winning section

--- Board.java
+++ Board.java
@@ -33,40 +33,62 @@ public class Board{
     public  boolean chechWinner(){
         if(board[0][0] == board[0][1] &&
            board[0][1] == board[0][2] &&
-           board[0][0] != '-')
+           board[0][0] != '-'
+        ){
+            return true;
+        }
                 
            if(board[1][0] == board[1][1] &&
               board[1][1] == board[1][2] && 
-              board[1][0] != '-')
+              board[1][0] != '-'
+            ) {
+                return true;
+            }
 
               if(board[2][0] == board[2][1] && 
                  board[2][1] == board[2][2] && 
-                 board[2][0] != '-')
+                 board[2][0] != '-'
+                ) {
+                    return true;
+                }
 
                  if(board[0][0] == board[1][0] && 
                     board[1][0] == board[2][0] && 
                     board[0][0] != '-'
-                 )
+                 ) {
+                    return true;
+                 }
                  
-                 if(board[0][1] == board[0][2] && 
-                    board[0][2] == board[2][2] && 
-                    board[0][1] != '-')
+                 if(board[0][1] == board[1][1] && 
+                    board[1][1] == board[2][1] && 
+                    board[0][1] != '-'
+                   ) {
+                    return true;
+                   }
 
-                    if(board[1][0] == board[1][1] && 
-                       board[1][1] == board[1][2] &&
-                       board[1][2] != '-')
+                    if(board[0][2] == board[1][2] && 
+                       board[1][2] == board[2][2] &&
+                       board[1][2] != '-'
+                    ) {
+                        return true;
+                    }
 
-                       if(board[2][0] == board[2][1] && 
-                          board[2][1] == board[2][2] &&
-                          board[2][0] != '-')
+                       
                           
                           if(board[0][0] == board[1][1] && 
                              board[1][1] == board[2][2] && 
-                             board[0][0] != '-')
+                             board[0][0] != '-'
+                            ){
+                                return true;
+                            }
 
-           {
-                return true;
-            }
+                             if(board[0][2] == board[1][1] && 
+                                board[1][1] == board[2][0] && 
+                                board[0][2] != '-'
+                            )  {
+                                return true;
+                            }
+
 
         
          return false;
