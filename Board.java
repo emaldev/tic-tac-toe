ed51f5e6fd659e146f4bd3ebce75507d8d44c0ee
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-09 07:22:56 +0330

    java game add the second solum

--- Board.java
+++ Board.java
@@ -47,6 +47,14 @@ public class Board{
                     board[1][0] == board[2][0] && 
                     board[0][0] != '-'
                  )
+                 
+                 if(board[0][1] == board[0][2] && 
+                    board[0][2] == board[2][2] && 
+                    board[0][1] != '-')
+
+                    if(board[1][0] == board[1][1] && 
+                       board[1][1] == board[1][2] &&
+                       board[1][2] != '-')
 
            {
                 return true;
