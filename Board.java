3931f87a5af32a34b8d97a34414ed6b9db8e9f5c
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-09 07:00:02 +0330

    java game Add the second and third rows and the left column.

--- Board.java
+++ Board.java
@@ -33,7 +33,22 @@ public class Board{
     public  boolean chechWinner(){
         if(board[0][0] == board[0][1] &&
            board[0][1] == board[0][2] &&
-           board[0][0] != '-'){
+           board[0][0] != '-')
+                
+           if(board[1][0] == board[1][1] &&
+              board[1][1] == board[1][2] && 
+              board[1][0] != '-')
+
+              if(board[2][0] == board[2][1] && 
+                 board[2][1] == board[2][2] && 
+                 board[2][0] != '-')
+
+                 if(board[0][0] == board[1][0] && 
+                    board[1][0] == board[2][0] && 
+                    board[0][0] != '-'
+                 )
+
+           {
                 return true;
             }
 
