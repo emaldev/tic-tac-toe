2d47e6357be6e9df9c40aaf13a7c17f69dbbae16
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-09 07:27:36 +0330

    java game add the main diagonal

--- Board.java
+++ Board.java
@@ -59,6 +59,10 @@ public class Board{
                        if(board[2][0] == board[2][1] && 
                           board[2][1] == board[2][2] &&
                           board[2][0] != '-')
+                          
+                          if(board[0][0] == board[1][1] && 
+                             board[1][1] == board[2][2] && 
+                             board[0][0] != '-')
 
            {
                 return true;
